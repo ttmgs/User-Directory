7823e7db8fd30becc87968586266049c4174839e
Magdi Tiea <magditiea@gmail.com>
2021-04-13 18:09:53 -0400

    edits

---
+++ app/src/components/click.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+class table extends React.Component {
+  render() {
+    return (
+    <div className="country-names">
+      <li>Canada</li>
+      <li>Australia</li>
+      <li>Norway</li>
+      <li>Mexico</li>
+    </div>
+    )
+  }
+}
