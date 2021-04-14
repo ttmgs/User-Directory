93757d81b8fe0384a00c9eb0211e05fa6c101dbc
Magdi Tiea <magditiea@gmail.com>
2021-04-14 14:37:11 -0400

    added an event listener to index.js/imported a function from app.js

---
+++ app/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Card from "./public/components/click"
+
+
+function App() {
+  return <Card />
+}
+
+export default App
---
+++ app/src/public/components/click.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+function Card() {
+  
+}
+
+export default Card;
